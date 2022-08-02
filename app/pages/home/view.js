2fe46741de65f1ab66320182e497df42acd074d5
Facundo Corigliano <facundo.corigliano@mercadolibre.com>
2022-08-02 12:40:11 -0300

    fix

--- app/pages/home/view.js
+++ app/pages/home/view.js
@@ -3,11 +3,7 @@ const React = require('react');
 function View(props){
   const { message } = props;
 
-  return(
-    <>
-    <h2>{message}</h2>
-    </>
-  )
+  return <h2>{message}</h2>
 }
 
 module.exports = View
